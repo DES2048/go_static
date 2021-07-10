f043e554bcc53939b523b5360781e8f32cc15c3e
Oleg Podzorov <electrochange@mail.ru>
2021-07-10 09:00:41 +0300

    refactor: render list in separate func

--- internal/mp4server/echo_server.go
+++ internal/mp4server/echo_server.go
@@ -4,11 +4,8 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"path"
 	"path/filepath"
-	"sort"
 
-	"github.com/dustin/go-humanize"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -27,43 +24,12 @@ func NewEchoServer(config *ServerConfig) (*EchoServer, error) {
 	}))
 
 	e.GET("/", func(c echo.Context) error {
-		videos, err := ListMp4(config.StaticFolder)
 
-		if err != nil {
-			return err
-		}
-
-		// sort by modTime
-		sort.Sort(
-			sort.Reverse(Mp4ByModTime(videos)),
+		return RenderMp4ListTemplate(
+			config,
+			c.Response(),
+			"",
 		)
-
-		videosTmpl := make([]VideoData, 0, len(videos))
-
-		for _, v := range videos {
-			videosTmpl = append(videosTmpl,
-				VideoData{
-					Url:      path.Join(config.StaticPath, v.Name()),
-					Title:    v.Name(),
-					Size:     humanize.Bytes(uint64(v.Size())),
-					Time:     humanize.Time(v.ModTime()),
-					Duration: v.Duration.String(),
-				},
-			)
-		}
-
-		data := &struct {
-			Videos []VideoData
-		}{
-			Videos: videosTmpl,
-		}
-
-		err = config.ListTemplate.Execute(c.Response(), data)
-		if err != nil {
-			return err
-		}
-
-		return nil
 	})
 
 	e.DELETE("/d/:file", func(c echo.Context) error {
